ad753f6f1032217342718c999885b4eb15d519c6
Jose Nunez-Yanez <eejlny@bristol.ac.uk>
2022-01-13 08:51:59 +0000

    update4

--- fused/PL/src/kernelMatrixmult.cpp
+++ fused/PL/src/kernelMatrixmult.cpp
@@ -1,4 +1,4 @@
-Copyright (c) [2021] [Jose Nunez-Yanez (eejlny@bristol.ac.uk)]
+/*Copyright (c) [2021] [Jose Nunez-Yanez (eejlny@bristol.ac.uk)]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
